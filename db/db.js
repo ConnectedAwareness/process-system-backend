62952e1401209aa46b64a9c664d1169062a3c14a
AmmonStretz <ammon-mino@web.de>
2018-04-01 16:35:31 +0200

    login works

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = (() => {
-    mongoose.connect('mongodb://localhost/conawa')
+    mongoose.connect('mongodb://localhost/test')
 
     let db = mongoose.connection
     db.on('error', console.error.bind(console, 'connection error:'))
