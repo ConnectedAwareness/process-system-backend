ffff4b546160a31e881b9c5132839ba0f2417550
jacobi <frieder.jacobi@googlemail.com>
2018-10-03 18:05:22 +0200

    MINOR follow-up to previous commit

--- src/features/processDoc/database/providers/node.providers.ts
+++ src/features/processDoc/database/providers/node.providers.ts
@@ -1,16 +1,17 @@
 import { Connection } from 'mongoose';
 import { TreeNodeSchema } from '../schemas/treenode.schema';
 import { LinkedNodeSchema } from '../schemas/linkednode.schema';
+import { TokenNames, SchemaTypeNames } from '../../../../common/util/constants';
 
 export const nodeProviders = [
-  {
+  { // NOTE maybe not needed
     provide: 'TreeNodeModelToken',
     useFactory: (connection: Connection) => connection.model('TreeNode', TreeNodeSchema),
     inject: ['DbConnectionToken'],
   },
   {
-    provide: 'LinkedNodeModelToken',
-    useFactory: (connection: Connection) => connection.model('LinkedNode', LinkedNodeSchema),
-    inject: ['DbConnectionToken'],
+    provide: TokenNames.LinkedNodeModelToken,
+    useFactory: (connection: Connection) => connection.model(SchemaTypeNames.LinkedNode, LinkedNodeSchema),
+    inject: [TokenNames.DbConnectionToken],
   },
 ];
